da037f0ba0a72887d73b819c2584c8269f9291bb
Gautham <58910261+Gautham-N@users.noreply.github.com>
2022-07-07 12:55:02 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,21 +1,9 @@
 #include <iostream>
 #include "BatteryConditions.h"
 #include <assert.h>
-bool batteryIsOk(float temperature, float soc, float chargeRate)
-{
-    BatteryConditions BatteryConditionCheck;
-    if (BatteryConditionCheck.TemperatureCheck(temperature) && BatteryConditionCheck.SocCheck(soc)
-        && BatteryConditionCheck.ChargeRateCheck(chargeRate))
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
 int main()
 {
-    assert(batteryIsOk(25, 70, 0.7) == true);
-    assert(batteryIsOk(50, 85, 0) == false);
+BatteryConditions BatteryConditionCheck;
+    assert(BatteryConditionCheck.batteryIsOk(25, 70, 0.7) == true);
+    assert(BatteryConditionCheck.batteryIsOk(50, 85, 0) == false);
 }
