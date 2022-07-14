1870dff411e40af386ffdeb3456b44e860611552
Gautham <58910261+Gautham-N@users.noreply.github.com>
2022-07-14 16:42:39 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -4,6 +4,12 @@
 int main()
 {
 BatteryConditions BatteryConditionCheck;
-    assert(BatteryConditionCheck.batteryIsOk(25, 70, 0.7) == true);
+   assert(BatteryConditionCheck.batteryIsOk(25, 70, 0.7) == true);
     assert(BatteryConditionCheck.batteryIsOk(50, 85, 0) == false);
+    assert(BatteryConditionCheck.batteryIsOk(30, 70, 0.1) == true);
+    assert(BatteryConditionCheck.batteryIsOk(30, 70, 0.9) == false);
+    assert(BatteryConditionCheck.batteryIsOk(1, 70, 0.7) == true);
+    assert(BatteryConditionCheck.batteryIsOk(25, 78, 0.7) == true);
+    assert(BatteryConditionCheck.batteryIsOk(25, 70, 0.7) == true);
+    assert(BatteryConditionCheck.batteryIsOk(25, 70, 0.79) == true);
 }
