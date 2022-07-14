536218b08e61d06a88b22758324dfab452a8af3c
Gautham <58910261+Gautham-N@users.noreply.github.com>
2022-07-14 16:46:03 +0530

    Update BatteryConditions.cpp

--- BatteryConditions.cpp
+++ BatteryConditions.cpp
@@ -1,53 +1,81 @@
 #include "BatteryConditions.h"
 #include "iostream"
-
+#include "BatteryLimitParams.h"
 
 
 bool BatteryConditions::checkRange(float actualValue, float minValue, float maxValue)
 {
-    return (actualValue < minValue || actualValue > maxValue);
+	return (actualValue <= minValue || actualValue > maxValue);
+}
+
+void BatteryConditions::checkBreachLow(float actualValue, float minValue, float maxValue, BATTERY_PARAMETER parameter)
+{
+	float tolerance = (maxValue * 5) / 100;
+	if (actualValue > minValue && actualValue < (minValue + tolerance))
+	{
+		printWarnMessage(parameter, LOW);
+	}
+}
+
+void BatteryConditions::checkBreachHigh(float actualValue, float maxValue, BATTERY_PARAMETER parameter)
+{
+	float tolerance = (maxValue * 5) / 100;
+	if (actualValue >= (maxValue - tolerance) && actualValue <= maxValue)
+	{
+		printWarnMessage(parameter, HIGH);
+	}
+}
+
+void BatteryConditions::printWarnMessage(BATTERY_PARAMETER parameter, PARAMETER_BREACH_TYPE breachType)
+{
+	std::cout << messageWarn[breachType] << "_" << messageFactor[languageOption][parameter] << "_WARN\n";
+}
+
+void BatteryConditions::printErrorMessage(BATTERY_PARAMETER messageKey)
+{
+	std::cout << messageFactor[languageOption][messageKey] << " " << messageTrailing[languageOption] << "\n";
+}
+
+bool BatteryConditions::batteryTemperatureIsOk(float temperature)
+{
+	bool result = true;
+	checkBreachLow(temperature, temperature_min, temperature_max, BATTERY_TEMPERATURE);
+	checkBreachHigh(temperature, temperature_max, BATTERY_TEMPERATURE);
+	if (checkRange(temperature, temperature_min, temperature_max))
+	{
+		printErrorMessage(BATTERY_TEMPERATURE);
+		result = false;
+	}
+	return result;
+}
+
+bool BatteryConditions::batterySocIsOk(float soc)
+{
+	bool result = true;
+	checkBreachLow(soc, soc_min, soc_max, BATTERY_SOC);
+	checkBreachHigh(soc, soc_max, BATTERY_SOC);
+	if (checkRange(soc, soc_min, soc_max))
+	{
+		printErrorMessage(BATTERY_SOC);
+		result = false;
+	}
+	return result;
+}
+
+bool BatteryConditions::batteryChargeRateIsOk(float chargeRate)
+{
+	bool result = true;
+	checkBreachLow(chargeRate, chargeRate_min, chargeRate_max, BATTERY_CHARGE_RATE);
+	checkBreachHigh(chargeRate, chargeRate_max, BATTERY_CHARGE_RATE);
+	if (checkRange(chargeRate, chargeRate_min, chargeRate_max))
+	{
+		printErrorMessage(BATTERY_CHARGE_RATE);
+		result = false;
+	}
+	return result;
 }
 
 bool BatteryConditions::batteryIsOk(float temperature, float soc, float chargeRate)
 {
-    
-    return (TemperatureCheck(temperature) && SocCheck(soc)
-        && ChargeRateCheck(chargeRate));
-    
-}
-
-bool BatteryConditions::TemperatureCheck(int threshold)
-{
-    if (checkRange(threshold , 0 , 45)) {
-     std::cout << "Temperature out of range!\n";
-        return false;
-    }
-    else
-    {
-        return true;
-    }
-}
-
-bool BatteryConditions::SocCheck(int threshold)
-{
-    if (checkRange(threshold, 20, 80) ){
-        std::cout << "State of Charge out of range!\n";
-        return false;
-    }
-    else
-    {
-        return true;
-    }
-}
-
-bool BatteryConditions::ChargeRateCheck(int threshold)
-{
-    if (checkRange(threshold, 0,0.8) ){
-        std::cout << "Charge Rate out of range!\n";
-        return false;
-    }
-    else
-    {
-        return true;
-    }
+	return (batteryTemperatureIsOk(temperature) && batterySocIsOk(soc) && batteryChargeRateIsOk(chargeRate));
 }
